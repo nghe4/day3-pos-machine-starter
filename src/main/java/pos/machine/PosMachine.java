4d20917c17cc88d027beeb41c481ad5b3a9c52f7
HEI204 <rayng5260@protonmail.com>
2025-09-03 19:36:41 +0800

    feat(PosMachine): add barcode validation method

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -3,7 +3,18 @@ package pos.machine;
 import java.util.List;
 
 public class PosMachine {
+    private static boolean isBarcodeValid(List<String> barcodes) {
+        for (String barcode : barcodes) {
+            if (barcode == null && barcode.trim().isEmpty())
+                return false;
+        }
+        return true;
+    }
+
     public String printReceipt(List<String> barcodes) {
+        if (!isBarcodeValid(barcodes)) {
+            System.out.println("Invalid barcodes input");
+        }
         return null;
     }
 }
